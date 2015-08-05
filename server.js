a8875aa392e6539c453afd1ec4644bbdd66b5d5d
Joe Longstreet <joelongstreet@gmail.com>
2015-08-05 16:46:13 -0500

    use environment variables to mask tokens

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+var env = require('../env-config.js')();
 var spark = require('sparknode');
 var apn = require('apn');
 
 var core = new spark.Core({
-  accessToken: "4b739ed05c46656233f63d3521129f0777ffe3bd",
-  id: "55ff6d065075555327220287"
+  accessToken: env.SPARK_CORE_TOKEN,
+  id: env.SPARK_CORE_ID
 });
 
 core.on('notifyWatch', function(info) {
@@ -14,21 +15,17 @@ note.badge = 3;
 note.sound = "ping.aiff";
 note.alert = "Its On!";
 
-apnConnection.pushNotification(note, myDevice);
+
+var apnConnection = new apn.Connection(options);
+var iosDevice = new apn.Device(env.IOS_TOKEN);
+apnConnection.pushNotification(note, iosDevice);
   console.log(info);
   console.log(info.data);
 })
 
-core.on('error', function(info)	{
+core.on('error', function(err){
+  console.log(err);
 });
-
-
-var options = { };
-
-var apnConnection = new apn.Connection(options);
-
-var myDevice = new apn.Device("d7a9d3f9 9fc48fe9 12123193 d35381b1 c04f5119 a2b99ef6 f9ee3783 21abb2c1");
-
 
 
 
